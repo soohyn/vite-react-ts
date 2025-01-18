b0e2eef0177685b1c64cb65ddc98fb837c2866a9
soohyn <sooheyonjo@gmail.com>
2025-01-19 00:41:19 +0900

    [구성] 절대 경로
    - resolve.alias 설정
    - tsconfig.app.json 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from 'node:url';
 
 const viteConfig = defineConfig({
   plugins: [
     react({
-      jsxRuntime: 'automatic',
+      jsxRuntime: "automatic",
     }),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   server: {
-    host: 'localhost',
+    host: "localhost",
     port: 3000,
   },
   preview: {
-    host: 'localhost',
+    host: "localhost",
     port: 8080,
   },
   css: {
